27ef61285ea20aafd9106d2f2fa3aed3d1af0f07
ISMAIL <laineis294@gmail.com>
2022-11-07 01:56:36 +0100

    gestion admin

---
+++ admin/view/DeleteAdmin.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+if(isset($_GET['id'])){
+    $id = $_GET["id"];
+    if(is_numeric($id))
+    {
+        require("./../../config.php");         
+        $query = "DELETE from admin where admin_id=:id";
+        $stmt = $db->prepare($query);
+        $stmt->execute(array(":id"=> $id)); // execute fkolchi
+
+        $_SESSION['status'] = 'Admin est Supprimer';
+        $_SESSION['status_code'] = 'success';
+        header("Location:GestionAdmin.php");
+    }else{
+        $_SESSION['status'] = 'Admin n\'exist pas ';
+        $_SESSION['status_code'] = 'error';
+        header("Location:GestionAdmin.php");
+    }
+}
